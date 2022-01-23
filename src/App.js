d0c94300bb4e29e3cd02b493b3cfcc9293dc9185
ivan schlagheck <ivansch05@gmail.com>
2022-01-23 12:48:18 +0100

    aded button functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+
+  const toggleParagraphHandler = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph);
+  };
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is New!</p>}
+      <Button onClick= {toggleParagraphHandler}>Toggle Paragraph!</Button>
     </div>
   );
 }

861c9420799299ff9ff4fc15106db751f18e5005
ivan schlagheck <ivansch05@gmail.com>
2022-01-23 13:35:08 +0100

    Implementing ReactMemo

--- src/components/UI/Button/Demo/DemoOutput.js
+++ src/components/UI/Button/Demo/DemoOutput.js
@@ -1,6 +1,10 @@
 import React from "react";
+import MyParagraph from "./MyParagraph";
 
 const DemoOutput = (props) => {
-  return <p>{props.show ? 'This is New!' : ''}</p> // Instead of conditionally rendering the whole html element, I condtition the Text inside checking if props.show is truethy
+  return <MyParagraph>{props.show ? 'This is New!' : ''}</MyParagraph> // Instead of conditionally rendering the whole html element, 
+                                                  //I condtition the Text inside checking if props.show is truethy
 }
-export default DemoOutput;
+export default React.memo(DemoOutput); // Memo tells react to check if the proips of this component have changed since the last state
+                                      //this way will not be rerender if it was not changed
+                                      
